90eea79f1f803ef5613ca4f6e1da4d122e248fae
Oleksiy Kachynskyy <16646570+lesyk-lesyk@users.noreply.github.com>
2024-06-20 16:18:02 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+
+async function run() {
+  try {
+    const name = core.getInput('username');
+    console.log(`Hello, ${name}!`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
